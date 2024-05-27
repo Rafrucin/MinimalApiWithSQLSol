9319561121da2f41e7104d20d84eaa44588ff0d3
Raf Rucinski <rafal_rucinski@poczta.fm>
2024-05-27 20:13:08 +0100

    Add project files.

---
+++ MinimalApiWithSQL/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiWithSQL.Entities;
+
+namespace MinimalApiWithSQL.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ MinimalApiWithSQL/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApiWithSQL.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=defaultConnection"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+
